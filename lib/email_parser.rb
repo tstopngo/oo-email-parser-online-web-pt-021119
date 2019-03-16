4d01168993743a42d328fe675ea101579cd64111
Tracy Ngo <tngo1206@gmail.com>
2019-03-16 21:12:18 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,12 @@ class EmailParser
   def initialized(emails)
     @emails = emails
   end
-    
+  
+  def parse
+    emails.split.collect do |address|
+      address.split(',') 
+    end.flatten.uniq
+  end
 
 
 end
